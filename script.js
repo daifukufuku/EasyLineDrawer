2531eb1096d4c865646d5f575705d43e7ad52e01
DFK <overeating19u@gmail.com>
2023-05-10 09:13:35 +0900

    remove comment

--- script.js
+++ script.js
@@ -7,14 +7,11 @@ function setCanvasSize(width, height) {
 }
 
 function drawLine(percentage, lineWidth) {
-    // 描画する線の長さを計算
     const lineLength = canvas.width * (percentage / 100);
 
-    // 描画設定
     ctx.lineWidth = lineWidth;
     ctx.strokeStyle = 'black';
-
-    // 線を描画
+    
     ctx.beginPath();
     ctx.moveTo(0, canvas.height / 2);
     ctx.lineTo(lineLength, canvas.height / 2);
